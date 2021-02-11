e2fbf95d5508b78069cf077982dc536aff074f9c
Juli <jwendt91@web.de>
2021-02-11 10:57:39 +0100

    array changes

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const colorBtn = document.querySelector('.colorBtn');
 const bodyBcg = document.querySelector('body');
 
 const newColors = [
-    ['yellow', '#ffff4d', '#ffd500'], 
-    ['red', '#ff4d4d', '#800000'], 
-    ['blue', '#add8e6', '#0000b3']
-]
+    [
+        ['#ff0000', '#ff3376'],
+        ['#ff7a7a', '#ffc0cb', '#b19cd9'],
+        ['#8b0000', '#d11d53'],
+    ], 
+    [
+        ['#0000ff', '#00ee00'],
+        ['#bfefff', '#00ff7f'],
+        ['#00008b', '#228b22'],
+    ]
+];
 
 
 colorBtn.addEventListener('click', changeColor);
 
 function changeColor() {
-    let primaryColor = Math.floor(Math.random()*newColors.length);
-    let randomColor = Math.floor(Math.random()*newColors[primaryColor].length);
-    bodyBcg.style.backgroundColor = newColors[primaryColor][randomColor];
+    const firstOrSecond = Math.floor(Math.random()*newColors.length);
+    const primarySet = Math.floor(Math.random()*newColors[firstOrSecond].length);
+    const finalColor = Math.floor(Math.random()*newColors[firstOrSecond][primarySet].length) 
+    bodyBcg.style.backgroundColor = newColors[firstOrSecond][primarySet][finalColor];
 }
